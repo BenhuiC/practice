6139424af81450312df6eb465a187684673251c9
yuanbenhui <yuanbenhui@senseauto.com>
2022-10-19 17:52:40 +0800

    10.19

--- 983_test.go
+++ 983_test.go
@@ -12,7 +12,23 @@ func Test_mincostTickets(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				days:  []int{1, 4, 6, 7, 8, 20},
+				costs: []int{2, 7, 15},
+			},
+			want: 11,
+		},
+		{
+			name: "2",
+			args: args{
+				days:  []int{1, 4, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 27, 28},
+				costs: []int{3, 13, 45},
+			},
+			want: 44,
+			//
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
