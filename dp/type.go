6babfd368c1c43f2406b0a3474bea2203a522823
yuanbenhui <yuanbenhui@senseauto.com>
2023-09-07 18:43:41 +0800

    907

--- dp/type.go
+++ dp/type.go
@@ -1,5 +1,18 @@
 package dp
 
+func Mins(nums ...int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	res := nums[0]
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < res {
+			res = nums[i]
+		}
+	}
+	return res
+}
+
 func Min(x, y int) int {
 	if x < y {
 		return x
