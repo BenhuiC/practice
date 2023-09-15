2c8542e468101823cf8d53ad8a4f3044b370818a
yuanbenhui <yuanbenhui@senseauto.com>
2023-09-15 10:20:31 +0800

    915

--- greedy/435.go
+++ greedy/435.go
@@ -2,31 +2,22 @@ package greedy
 
 import "sort"
 
-// not pass
 func eraseOverlapIntervals(intervals [][]int) int {
 	n := len(intervals)
 	if n < 2 {
 		return 0
 	}
 	sort.Slice(intervals, func(i, j int) bool {
-		if intervals[i][0] != intervals[j][0] {
-			return intervals[i][0] < intervals[j][0]
-		} else {
-			return intervals[i][1] < intervals[j][1]
-		}
+		return intervals[i][1] < intervals[j][1]
 	})
 
-	res := 0
-	l, r := 0, 1
-	for l < r && r < n {
-		if intervals[r][0] < intervals[l][1] {
-			r++
-			res++
-		} else {
-			l = r
-			r++
+	ln, right := 1, intervals[0][1]
+	for i := 1; i < n; i++ {
+		if intervals[i][0] >= right {
+			ln++
+			right = intervals[i][1]
 		}
 	}
 
-	return res
+	return n - ln
 }
