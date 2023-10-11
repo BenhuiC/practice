dd422032c85f38ade177d9515e8888524f0291d5
yuanbenhui <yuanbenhui@senseauto.com>
2023-10-11 23:21:26 +0800

    1011

---
+++ offer/20.go
@@ -0,0 +1,27 @@
+package offer
+
+func isValid(s string) bool {
+	st := make([]byte, 0)
+	for _, v := range s {
+		switch v {
+		case '(', '[', '{':
+			st = append(st, byte(v))
+		case ')':
+			if len(st) == 0 || st[len(st)-1] != '(' {
+				return false
+			}
+			st = st[:len(st)-1]
+		case ']':
+			if len(st) == 0 || st[len(st)-1] != '[' {
+				return false
+			}
+			st = st[:len(st)-1]
+		case '}':
+			if len(st) == 0 || st[len(st)-1] != '{' {
+				return false
+			}
+			st = st[:len(st)-1]
+		}
+	}
+	return len(st) == 0
+}
