462679abe609c0bf6550948ec84d23e1df3d360b
yuanbenhui <yuanbenhui@senseauto.com>
2023-11-12 22:05:09 +0800

    1112

---
+++ offer/448.go
@@ -0,0 +1,18 @@
+package offer
+
+import "fmt"
+
+func findDisappearedNumbers(nums []int) []int {
+	res := make([]int, 0)
+	for i := 0; i < len(nums); i++ {
+		for nums[nums[i]-1] != nums[i] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+		}
+	}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != i+1 {
+			res = append(res, i+1)
+		}
+	}
+	return res
+}
