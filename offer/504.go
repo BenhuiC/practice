f70330c1b09889a7fd04b65dc0ac18884cb8c2b7
yuanbenhui <yuanbenhui@senseauto.com>
2023-11-26 22:46:36 +0800

    1126

---
+++ offer/504.go
@@ -0,0 +1,21 @@
+package offer
+
+func convertToBase7(num int) string {
+	if num==0{
+		return "0"
+	}
+	nagivate:=false
+	if num<0{
+		nagivate=true
+		num=-num
+	}
+	res:=make([]byte,0)
+	for num!=0{
+		res=append([]byte{'0'+byte(num%7)},res...)
+		num/=7
+	}
+	if nagivate{
+		res=append([]byte{'-'},res...)
+	}
+	return string(res)
+}
