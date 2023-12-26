3dabfce470ecc97bc14e0bc1031f0a6b7f43e888
yuanbenhui <yuanbenhui@senseauto.com>
2023-12-26 16:43:43 +0800

    1226

---
+++ offer/684_test.go
@@ -0,0 +1,32 @@
+package offer
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findRedundantConnection(t *testing.T) {
+	type args struct {
+		edges [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				edges: [][]int{{1, 2}, {1, 3}, {2, 3}},
+			},
+			want: []int{1, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findRedundantConnection(tt.args.edges); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findRedundantConnection() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
