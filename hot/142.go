dd7f7aaf25bdfb2103c61cbb02d8b0bbf955c155
yuanbenhui <yuanbenhui@senseauto.com>
2024-02-11 11:47:18 +0800

    0211

--- hot/142.go
+++ hot/142.go
@@ -1,5 +1,29 @@
 package hot
 
 func detectCycle(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for slow != nil && fast != nil {
+		if fast.Next != nil {
+			fast = fast.Next.Next
+		} else {
+			break
+		}
+		slow = slow.Next
+		if slow == fast {
+			break
+		}
+	}
+	if slow == nil || fast == nil || fast.Next == nil {
+		return nil
+	}
 
+	fast = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
 }
