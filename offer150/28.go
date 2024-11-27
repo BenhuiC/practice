f95a60bcbeb27ddb9d9df1e7b55ff89bf1b00a44
yuanbenhui <yuanbenhui@senseauto.com>
2024-11-27 16:23:35 +0800

    1127

--- offer150/28.go
+++ offer150/28.go
@@ -1,1 +1,14 @@
 package offer150
+
+func strStr(haystack string, needle string) int {
+	m, n := len(haystack), len(needle)
+	if m < n {
+		return -1
+	}
+	for i := 0; i <= m-n; i++ {
+		if haystack[i:i+n] == needle {
+			return i
+		}
+	}
+	return -1
+}
--- offer150/6.go
+++ offer150/6.go
@@ -1,1 +1,24 @@
 package offer150
+
+func convert(s string, numRows int) string {
+	if len(s) <= numRows || 1 == numRows {
+		return s
+	}
+	step := 2*numRows - 2
+	level := 0
+	res := ""
+	for level = 0; level < numRows; level++ {
+		lastStep := step - 2*level
+		idx := level
+		for idx < len(s) {
+			if lastStep == 0 {
+				lastStep = step
+				continue
+			}
+			res += string(s[idx])
+			idx += lastStep
+			lastStep = step - lastStep
+		}
+	}
+	return res
+}
